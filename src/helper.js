41388768d4b694ad273ce5a129e23a993cac09f8
BalavigneshSankar <balavignesh.shankar@gmail.com>
2023-07-28 18:03:13 +0530

    update helper.js

--- src/helper.js
+++ src/helper.js
@@ -1,4 +1,5 @@
-export const URL = "http://localhost:5000";
+// export const URL = "http://localhost:5000";
+export const URL = "https://amazon-minified-node-js.onrender.com";
 
 export const createConfigObj = () => {
   const token = localStorage.getItem("token");
